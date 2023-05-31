5c7d2c348d28e7e83518b25e52d0984bdf0eba62
jose angel coronado <joseenan2777678@gmail.com>
2023-05-30 21:10:34 -0400

    agregando componente item
    plantilla de productos

---
+++ src/components/Item/item.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Item = ({item}) => {
+    return (
+
+  <div className="card" style={{width: '18rem'}} data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+    <img src={`./img/${item.img}`} alt={`${item.nombre}`} />
+    <div className="card-body collapse" id="collapseExample">
+      <p className="card-text">{item.descripcion}</p>
+    </div>
+      <h5 className="card-title">{item.nombre} {item.modelo}</h5>
+      <p className="card-text">{item.numeroDArticulo}</p>
+      <p className="card-text">{item.precio}</p>
+      <a href="#" className="btn btn-primary">Compra</a>
+  </div>
+
+    );
+}
+
+export default Item;
